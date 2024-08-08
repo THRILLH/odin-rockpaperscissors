87134eb388afd31d83840fe1de30ae6b59683f99
Matt Seton <mmseton@gmail.com>
2024-08-08 19:48:41 -0300

    Create function for human choice and print in console

--- javascript.js
+++ javascript.js
@@ -1,9 +1,20 @@
 
 function getComputerChoice() {
-    let randomChoice = Math.floor(Math.random() * 3) + 1
-    if (randomChoice == 1) return "Rock"
-    if (randomChoice == 2) return "Paper"
-    if (randomChoice == 3) return "Scissors"
+    let randomChoice = Math.floor(Math.random() * 3) + 1;
+    if (randomChoice == 1) return "Rock";
+    if (randomChoice == 2) return "Paper";
+    if (randomChoice == 3) return "Scissors";
 }
+
+function getHumanChoice() {
+    let humanChoice = prompt("Press 1 for rock, 2 for paper, or 3 for scissors");
+    if (humanChoice === "1") return console.log("You chose rock");
+    if (humanChoice === "2") return console.log("You chose paper");
+    if (humanChoice === "3") return console.log("You chose scissors");
+    else return console.log("Please type only 1, 2, or 3");
+
+}
+
+getHumanChoice();
 
 console.log(getComputerChoice());
