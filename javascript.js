53c65e38a0afeca828091c0b6f0064c97b8f8d44
Matt Seton <mmseton@gmail.com>
2024-08-09 11:04:57 -0300

    Create playRound function which runs choices

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-
+// Generate the computer's choice
 let computerChoice;
 function getComputerChoice() {
     let randomChoice = Math.floor(Math.random() * 3) + 1;
@@ -7,6 +7,7 @@ function getComputerChoice() {
     if (randomChoice == 3) return computerChoice = "scissors";
 }
 
+// Generate a prompt to get the human's choice
 let humanChoice;
 function getHumanChoice() {
     let humanChoicePrompt = prompt("Press 1 for rock, 2 for paper, or 3 for scissors");
@@ -17,11 +18,19 @@ function getHumanChoice() {
 
 }
 
+// Create the scores
 let humanScore = 0;
 let computerScore = 0;
 
+// Play a single round
+function playRound(human, computer) {
 getComputerChoice();
 getHumanChoice();
+    if (humanChoice === "rock" && computerChoice === "rock") return console.log("Draw. You both chose rock.");
 
+}
+playRound(humanChoice, computerChoice);
+
+// Show the choices in console
 console.log("You chose " + humanChoice);
 console.log("The computer chose " + computerChoice);
