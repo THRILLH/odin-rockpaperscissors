035ea6467bb44d568328350c6ed1c1e7aee73f8c
Matt Seton <mmseton@gmail.com>
2024-08-09 10:33:48 -0300

    Change getHumanChoice to return rock, paper, or scissors string

--- javascript.js
+++ javascript.js
@@ -1,20 +1,22 @@
 
 function getComputerChoice() {
     let randomChoice = Math.floor(Math.random() * 3) + 1;
-    if (randomChoice == 1) return "Rock";
-    if (randomChoice == 2) return "Paper";
-    if (randomChoice == 3) return "Scissors";
+    if (randomChoice == 1) return "rock";
+    if (randomChoice == 2) return "paper";
+    if (randomChoice == 3) return "scissors";
 }
 
+let humanChoice;
 function getHumanChoice() {
-    let humanChoice = prompt("Press 1 for rock, 2 for paper, or 3 for scissors");
-    if (humanChoice === "1") return console.log("You chose rock");
-    if (humanChoice === "2") return console.log("You chose paper");
-    if (humanChoice === "3") return console.log("You chose scissors");
+    let humanChoicePrompt = prompt("Press 1 for rock, 2 for paper, or 3 for scissors");
+    if (humanChoicePrompt === "1") return humanChoice = "rock";
+    if (humanChoicePrompt === "2") return humanChoice = "paper";
+    if (humanChoicePrompt === "3") return humanChoice = "scissors";
     else return console.log("Please type only 1, 2, or 3");
 
 }
 
 getHumanChoice();
 
-console.log(getComputerChoice());
+console.log("You chose " + humanChoice);
+console.log("The computer chose " + getComputerChoice());
