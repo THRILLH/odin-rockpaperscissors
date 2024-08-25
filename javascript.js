617918d11f4e0e4790d3256498250aa99ae04711
Matt Seton <mmseton@gmail.com>
2024-08-25 09:38:30 -0300

    Create function to display result of each round and the current scores

--- javascript.js
+++ javascript.js
@@ -1,4 +1,3 @@
-// Generate the computer's choice
 let computerChoice;
 function getComputerChoice() {
     let randomChoice = Math.floor(Math.random() * 3) + 1;
@@ -7,48 +6,46 @@ function getComputerChoice() {
     if (randomChoice == 3) return computerChoice = "scissors";
 }
 
-// Create the scores
 let humanScore = 0;
 let computerScore = 0;
 
 const resultsDiv = document.querySelector(".results");
 const result = document.createElement("p");
 
+const scoreDiv = document.querySelector(".score");
+const score1 = document.createElement("p");
+const score2 = document.createElement("p");
+
+function showResults() {
+    resultsDiv.appendChild(result);
+    score1.textContent = `Your score: ${humanScore}`;
+    scoreDiv.appendChild(score1);
+    score2.textContent = `Computer score: ${computerScore}`;
+    scoreDiv.appendChild(score2);
+}
+
+
 // Play a single round
 function playRound(humanChoice) {
 getComputerChoice();
-console.log("Human: " + humanChoice);
-console.log("Computer: " + computerChoice);
 if (humanChoice === computerChoice) {
     result.textContent = `It's a draw! You both chose ${humanChoice}.`; 
-    resultsDiv.appendChild(result);
+    showResults();
 }
 else if ((humanChoice === "rock" && computerChoice === "scissors") || (humanChoice === "paper" && computerChoice === "rock") || (humanChoice === "scissors" && computerChoice === "paper")) {
     humanScore++
     result.textContent = `You win! ${humanChoice} beats ${computerChoice}.`; 
-    resultsDiv.appendChild(result);
+    showResults();
 }
 else if ((humanChoice === "rock" && computerChoice === "paper") || (humanChoice === "paper" && computerChoice === "scissors") || (humanChoice === "scissors" && computerChoice === "rock")) {
     computerScore++
     result.textContent = `You lose! ${computerChoice} beats ${humanChoice}.`; 
-    resultsDiv.appendChild(result);
+    showResults();
 }
 else return console.log("Please type only 1, 2, or 3");
 }
 
-// Function to play 5 rounds
-function playGame() {
-    playRound();
-    playRound();
-    playRound();
-    playRound();
-    playRound();
-    if (humanScore > computerScore) return console.log("You won the game!");
-    else if (computerScore > humanScore) return console.log("You lost the game!");
-    else if (computerScore === humanScore) return console.log("The game was a tie");
-}
-
-
+// Make the 3 buttons play one round
 const rockBtn = document.querySelector("#rock-btn");
 rockBtn.addEventListener("click", () => {
     playRound("rock",);
